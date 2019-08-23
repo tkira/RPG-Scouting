bcc2d77c03ecbc021528324599a90b00e3862134
ali <bahr0027@flinders.edu.au>
2019-08-23 13:16:24 +0930

    a

---
+++ Assets/Script/playercontroler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroler : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public Slider HealthBar;
+    public Slider Armor;
+    public GameObject player;
+    void Start()
+     {
+
+        rb = GetComponent<Rigidbody2D>();
+
+     }
+     void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+        //potion
+        //if (Input.GetKey(KeyCode.H))
+        //{
+        //    HealthBar.value -= 0.05f;
+        //}
+
+        if (HealthBar.value == 0.00f)
+        {
+            HealthBar.value += 1.00f;
+            dead();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Armor.value > 0.1f) {
+            if (other.CompareTag("bullet"))
+            {
+                HealthBar.value -= 0.05f;
+                Armor.value -= 0.2f;
+            }
+        }
+        else{
+            HealthBar.value -= 0.3f;
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+    void dead()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/screen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen : MonoBehaviour
+{
+    private Vector2 screenBounds;   
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x, screenBounds.x * -1);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y, screenBounds.y * -1);
+        transform.position = viewPos;
+    }
+}
