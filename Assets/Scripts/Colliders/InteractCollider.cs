7d4562fcd4e747afb2292ba53008de858fc1a9fa
ali <bahr0027@flinders.edu.au>
2019-08-28 01:16:24 +0930

    Walls and room/camera change

--- Assets/Scripts/Colliders/InteractCollider.cs
+++ Assets/Scripts/Colliders/InteractCollider.cs
@@ -1,31 +1,141 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InteractCollider : MonoBehaviour {
 
     public GameObject interactIcon;
-
+    public GameObject Portal;
+    public GameObject Player;
+    public GameObject DoorIcon;
+    bool Ttrigger = false;
+    bool Btrigger = false;
+    bool Rtrigger = false;
+    bool Ltrigger = false;
+    float xPos;
+    float yPos;
+    float zPos;
     //Enable object if in range
+
+
     void OnTriggerEnter2D (Collider2D col) {
         Debug.Log ("Hit Object");
+        //-----interactable icons
         if (col.gameObject.tag == "Interact") {
-            //Set object to active
             interactIcon.SetActive(true);
         }
-    }
+        //-----Door icon & triggers
+        if (col.gameObject.tag == "Top Door")
+        {
+            DoorIcon.SetActive(true);
+            Ttrigger = true;
+        }
+        if (col.gameObject.tag == "Right Door")
+        {
+            Rtrigger = true;
+            DoorIcon.SetActive(true);
+        }
+        if(col.gameObject.tag == "Bottom Door")
+        {
+            Btrigger = true;
+            DoorIcon.SetActive(true);
+        }
+        if(col.gameObject.tag == "Left Door")
+        {
+            Ltrigger = true;
+            DoorIcon.SetActive(true);
+        }
+     }
 
-    //Disable if exit 
+   
+//Disable if exit 
     void OnTriggerExit2D (Collider2D col) {
         Debug.Log ("Exit Object");
         if (col.gameObject.tag == "Interact") {
-            //Disable object
             interactIcon.SetActive(false);
+        }
+        //-----Door icon & triggers
+        if (col.gameObject.tag == "Top Door")
+        {
+            DoorIcon.SetActive(false);
+            Ttrigger = false;
+        }
+        if (col.gameObject.tag == "Right Door")
+        {
+            Rtrigger = false;
+            DoorIcon.SetActive(false);
+        }
+        if(col.gameObject.tag == "Bottom Door")
+        {
+            Btrigger = false;
+            DoorIcon.SetActive(false);
+        }
+        if (col.gameObject.tag == "Left Door")
+        {
+            Ltrigger = false;
+            DoorIcon.SetActive(false);
         }
     }
 
     // Update is called once per frame
     void Update () {
+        xPos = GameObject.Find("Player").transform.position.x;
+        yPos = GameObject.Find("Player").transform.position.y;
+        zPos = GameObject.Find("Player").transform.position.z;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Ttrigger == true)
+            {
+                movecameraup();
+                Debug.Log("Toptriggerworked");
+            }
+            if (Rtrigger == true)
+            {
+                movecameraright();
+                Debug.Log("Righttriggerworked");
+            }
+            if (Ltrigger == true)
+            {
+                movecameraleft();
+                Debug.Log("Lefttriggerworked");
+            }
+            if (Rtrigger == true)
+            {
+                movecameradown();
+                Debug.Log("Bottomtriggerworked");
+            }
+        }
 
     }
+
+    public void movecameraup()
+    {
+        GameObject.Find("Main Camera").transform.position = new Vector3((float)0.1, (float)16.5, (float)-10);
+
+        GameObject.Find("Player").transform.position = new Vector3(xPos, yPos += 4, zPos);
+    }
+    public void movecameraright()
+    {
+        GameObject.Find("Main Camera").transform.position = new Vector3(30, (float)-0.9, (float)-10);
+
+        GameObject.Find("Player").transform.position = new Vector3(xPos += 4, yPos, zPos);
+    }
+    public void movecameraleft()
+    {
+        GameObject.Find("Main Camera").transform.position = new Vector3(-30, (float)-0.9, (float)-10);
+
+        GameObject.Find("Player").transform.position = new Vector3(xPos -= 4, yPos, zPos);
+    }
+    public void movecameradown()
+    {
+        GameObject.Find("Main Camera").transform.position = new Vector3((float)0.1, (float)-16.5, (float)-10);
+
+        GameObject.Find("Player").transform.position = new Vector3(xPos, yPos -= 4, zPos);
+    }
+
+
+
+
 }
---
+++ Assets/Scripts/Door/Teleportation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Current;
+    float xPos;
+    float yPos;
+    float zPos;
+    bool trigger = false;
+
+    void Start()
+    {       
+        GameObject.Find("Main Camera").transform.position = new Vector3((float)0.1, (float)-0.9, (float)-10);
+    }
+
+    //----------------------
+    void Update()
+    {
+        xPos = GameObject.Find("Player").transform.position.x;
+        yPos = GameObject.Find("Player").transform.position.y;
+        zPos = GameObject.Find("Player").transform.position.z;   
+    }
+
+    //void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("player de");
+    //        if (Input.GetKeyDown(KeyCode.Space))
+    //        {
+    //            if (Current.gameObject.tag == "Top Door")
+    //            {
+    //                Debug.Log("door de");
+    //                movecameraup();
+    //            }
+                    
+    //        }
+    //    }
+    //}
+
+
+    //----------------------
+
+
+    public void movecameraup()
+    {
+        GameObject.Find("Main Camera").transform.position = new Vector3((float)0.1, (float)16.5, (float)-10);
+
+        GameObject.Find("Player").transform.position = new Vector3(xPos, yPos+=4, zPos);
+    }
+
+}
