358f70666b40b6c9100bcf8d63b80c5094c8e8a5
Matthew <43989103+Cpt-Archimedes@users.noreply.github.com>
2019-08-30 13:44:39 +0930

    RandomDungeon V1

---
+++ Assets/Scripts/AlreadyRoomHere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlreadyRoomHere : MonoBehaviour
+{
+    public int spawnVal;
+    public GameObject mapRoom;
+    public bool spawnMap = true;
+
+
+    void Start(){
+        if(spawnMap){
+            float y = (0.0313f * transform.position.y) + 3;
+            float x = (0.0313f * transform.position.x) + 5.5f;
+            Instantiate(mapRoom,new Vector3(x,y,0), Quaternion.identity);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other){
+
+        Destroy(other);
+
+    }
+}
---
+++ Assets/Scripts/RoomNotSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNotSpawner : MonoBehaviour
+{
+    public int closedDirection;
+    // 16 = Not Top
+    // 32 = Not Bottom
+    // 64 = Not Left
+    // 128 = Not Right
+
+    // Update is called once per frame
+
+    
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("AlreadyRoom")){
+
+            Destroy(gameObject);
+        }
+    }
+}
