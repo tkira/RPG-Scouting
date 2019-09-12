6a0d3dfebf249e407240e3041b081d9af2dcaba0
ali <bahr0027@flinders.edu.au>
2019-09-12 21:10:09 +0930

    UI and scene
    not fully finished commiting to save

---
+++ Assets/Cameracontroller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour
+{
+	Vector2 viewPortSize;
+	Camera cam;
+    public float viewPortFactor;
+    Vector3 targetPostion;
+    private Vector3 currentVelocity;
+    public float followDuration;
+    public float maximumFollowSpeed;
+    public Transform player;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        viewPortSize = (cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) - cam.ScreenToWorldPoint(Vector2.zero)) * viewPortFactor;
+        targetPostion = player.position - new Vector3(0,0,10);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPostion, ref currentVelocity, followDuration, maximumFollowSpeed);
+    }
+    public void OnDrawGizmos()
+    {
+        Color c = Color.red;
+        c.a = 0.3f;
+        Gizmos.color = c;
+        Gizmos.DrawCube(transform.position, viewPortSize);
+    }
+}
---
+++ Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour
+{
+
+    private bool pause = false;
+    public GameObject Resume;
+    public GameObject Controls;
+    public GameObject Options;
+    //public GameObject Confirm;
+    public GameObject Exit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Resume.SetActive(true);
+            Controls.SetActive(true);
+            Options.SetActive(true);
+            Exit.SetActive(true);
+
+            if (pause)
+            {
+                Time.timeScale = 1;
+            }
+            else
+            {
+                Time.timeScale = 0;
+            }
+        }
+    }
+    public void confirm()
+    {
+        Resume.SetActive(false);
+        Controls.SetActive(false);
+        Options.SetActive(false);
+        Exit.SetActive(false);
+        //Confirm.SetActive(true);
+    }
+    public void no()
+    {
+        Resume.SetActive(false);
+        Controls.SetActive(false);
+        Options.SetActive(false);
+        Exit.SetActive(false);
+        Time.timeScale = 1;
+        //Confirm.SetActive(false);
+        //BG.SetActive(false);
+        
+    }
+}
