61da58b8873818b2399efec28237c02ae4a8b0d3
Tung Dang <tung.dang.34@gmail.com>
2019-10-04 09:15:52 +0930

    Fixed

--- Assets/Scripts/Scouting/ScoutingController.cs
+++ Assets/Scripts/Scouting/ScoutingController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoutingController : MonoBehaviour
 {
+
+    public int scoutingLevel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,9 @@ public class ScoutingController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void gotoDungeon (){
+        SceneManager.LoadScene("Dungeon");
     }
 }
