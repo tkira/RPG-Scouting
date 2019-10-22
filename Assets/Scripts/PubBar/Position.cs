57b321d807484027343d8a70cfe90737dada75bc
ali <bahr0027@flinders.edu.au>
2019-10-23 03:13:26 +1030

    pub
    Pub

---
+++ Assets/Scripts/PubBar/Position.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+
+    [SerializeField]
+    private int sort = 5000;
+    [SerializeField]
+    private int offset = 0;
+    private Renderer renderer;
+    
+
+    private void Awake()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        renderer.sortingOrder = (int)(sort - transform.position.y - offset);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
