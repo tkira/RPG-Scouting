f134923cd2b393ef3fe5b0fc27f769faf8fd6edd
ali <bahr0027@flinders.edu.au>
2019-10-31 18:42:26 +1030

    egg and town update
    aa

--- Assets/Scripts/Shop/shopmanager.cs
+++ Assets/Scripts/Shop/shopmanager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class shopmanager : MonoBehaviour
 {
+    
+    public GameObject icon, shop, menuitem, diamond, title, price;
+    public bool trigger,pressed;
+    
+    
 
-    public GameObject icon, shop, menuitem;
-    public bool trigger;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,7 +32,7 @@ public class shopmanager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "";
     }
 
     // Update is called once per frame
@@ -44,6 +48,10 @@ public class shopmanager : MonoBehaviour
         {
             shop.SetActive(false);
         }
+        //if(pressed == true)
+        //{
+        //    diamond.GetComponentInChildren<TextMeshProUGUI>().text = "Ask For Help";
+        //}
     }
 
     public void back()
@@ -51,5 +59,42 @@ public class shopmanager : MonoBehaviour
         shop.SetActive(false);
         menuitem.SetActive(true);
         Time.timeScale = 1;
+    }
+
+    public void AC130()
+    {
+        price.GetComponentInChildren<TextMeshProUGUI>().text = "$2.50";
+        title.GetComponentInChildren<TextMeshProUGUI>().text = "AC130";
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "Call in a AC130 during combat to rain hell";
+    }
+    public void minecraft()
+    {
+        price.GetComponentInChildren<TextMeshProUGUI>().text = "$392.00";
+        title.GetComponentInChildren<TextMeshProUGUI>().text = "Diamond Sword";
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "diamond sword. Swords are weapons that are primarily used to kill mobs or players (if multiplayer PvP is on) quicker than with just your character's bare hands.";
+    }
+    public void rick()
+    {
+        price.GetComponentInChildren<TextMeshProUGUI>().text = "$0.20";
+        title.GetComponentInChildren<TextMeshProUGUI>().text = "Portal Gun";
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "designed to place two portals that objects can pass through.";
+    }
+    public void jugg()
+    {
+        price.GetComponentInChildren<TextMeshProUGUI>().text = "15-killstreak";
+        title.GetComponentInChildren<TextMeshProUGUI>().text = "Juggernaut";
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "Call in a Juggernaut Suit during combat which will protect you from explosive ordnance disposal armor, requiring excessive amounts of damage to kill.";
+    }
+    public void realsword()
+    {
+        price.GetComponentInChildren<TextMeshProUGUI>().text = "$19.99";
+        title.GetComponentInChildren<TextMeshProUGUI>().text = "Diamond Sword Pro";
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "Actual sword";
+    }
+    public void diamong()
+    {
+        price.GetComponentInChildren<TextMeshProUGUI>().text = "$100,000.00";
+        title.GetComponentInChildren<TextMeshProUGUI>().text = "ICED out 2-tone Patek";
+        diamond.GetComponentInChildren<TextMeshProUGUI>().text = "While in combat give a flick of the wrist and blind the enemey for 1.3 seconds";
     }
 }
