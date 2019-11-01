17eb01fc32b65f9a3e2d4ed7e99f6f393234532d
ali <bahr0027@flinders.edu.au>
2019-11-01 23:07:50 +1030

    ui
    ui

---
+++ Assets/BPbar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPbar : MonoBehaviour
+{
+    public GameObject btn1, btn2, bp;
+
+    public void openbp()
+    {
+        btn1.SetActive(false);
+        btn2.SetActive(true);
+        bp.SetActive(true);
+    }
+    public void closebp()
+    {
+        bp.SetActive(false);
+        btn2.SetActive(false);
+        btn1.SetActive(true);
+    }
+}
